391c6507e5ee3e408fa2bd02052ab2369544201b
NathanCollins4551 <162639301+NathanCollins4551@users.noreply.github.com>
2024-12-03 20:24:18 -0500

    Day 1 done

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,47 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => new($"{SolvePartOne()}");
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2() => new($"{SolvePartTwo()}");
+
+    public int SolvePartOne()
+    {
+        List<int> list1 = new List<int>();
+        List<int> list2 = new List<int>();
+        foreach (var line in input.Split("\n").Where(x => x.Length > 0))
+        {
+            list1.Add(Int32.Parse(line.Split("   ")[0]));
+            list2.Add(Int32.Parse(line.Split("   ")[1]));
+        }
+        list1 = list1.Order().ToList();
+        list2 = list2.Order().ToList();
+
+        int sum = 0;
+        for(int i = 0; i < list1.Count(); i++)
+        {
+            sum += Math.Abs(list1[i] - list2[i]);
+        }
+        return sum;
+    }
+
+    public int SolvePartTwo()
+    {
+        List<int> list1 = new List<int>();
+        List<int> list2 = new List<int>();
+        foreach (var line in input.Split("\n").Where(x => x.Length > 0))
+        {
+            list1.Add(Int32.Parse(line.Split("   ")[0]));
+            list2.Add(Int32.Parse(line.Split("   ")[1]));
+        }
+
+        return list1.Select(num => list2.Where(x => x.Equals(num)).Count() * num).Sum();
+    }
 }

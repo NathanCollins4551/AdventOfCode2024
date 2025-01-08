7b11dc673365a5de050fd06b5252501736e8ad54
NathanCollins4551 <162639301+NathanCollins4551@users.noreply.github.com>
2025-01-07 22:43:04 -0500

    Day 11 conquered

--- AdventOfCode/Day11.cs
+++ AdventOfCode/Day11.cs
@@ -5,58 +5,90 @@ namespace AdventOfCode;
 public class Day11 : BaseDay
 {
     private readonly string[] input;
-    int count;
-
+    Func<string, int, long> functionToRun;
+    Dictionary<(string, int), long> dict;
     public Day11()
     {
         input = File.ReadAllText(InputFilePath).Split(' ').Where(item => item.Length > 0).ToArray();
+        functionToRun = evalOptimized;
+        dict = new Dictionary<(string, int), long>();
     }
 
-    public override ValueTask<string> Solve_1() => new($"{SolvePartOne()}");
+    public override ValueTask<string> Solve_1() => new($"{SolvePartOneOptimized()}");
 
     public override ValueTask<string> Solve_2() => new($"{SolvePartTwo()}");
 
+    public long SolvePartOneOptimized()
+    {
+        long count = input.Length;
+        const int blinks =25;
+        foreach (var num in input)
+        {
+            count += dict.TryGetKey(num, blinks, functionToRun);
+        }
+        return count;
+    }
+    public long SolvePartTwo()
+    {
+        long count = input.Length;
+        const int blinks = 75;
+        foreach (var num in input)
+        {
+            count += dict.TryGetKey(num, blinks, functionToRun);
+        }
+        return count;
+    }
     public int SolvePartOne()
     {
-        count = 0;
+        int count = input.Length;
+        const int blinks = 25;
         foreach (var num in input)
         {
-            eval(num, 25);
-            count++;
+            count += eval(num, blinks);
         }
         return count;
     }
-
-    public int SolvePartTwo()
-    {
-        count = 0;
-        foreach (var num in input)
-        {
-            eval(num, 75);
-            count++;
-        }
-        return count;
-    }
-    public void eval(string num, int itr)
+    public long evalOptimized(string num, int itr)
     {
         if (itr <= 0)
         {
-            return;
+            return 0;
         }
         if (num.Equals("0"))
         {
-            eval("1", itr - 1);
+            return dict.TryGetKey("1", itr - 1, functionToRun);
+        }
+        else if (num.Length % 2 == 0)
+        {
+            return dict.TryGetKey(num.Substring(0, num.Length / 2), itr - 1, functionToRun) +
+                   dict.TryGetKey(Regex.Replace(num.Substring(num.Length / 2, num.Length / 2), "^0+(?!$)", ""), itr - 1, functionToRun) +
+                   1;
+        }
+        else
+        {
+            return dict.TryGetKey((long.Parse(num) * 2024).ToString(), itr - 1, functionToRun);
+        }
+    }
+    public int eval(string num, int itr)
+    {
+        if (itr <= 0)
+        {
+            return 0;
+        }
+        if (num.Equals("0"))
+        {
+            return eval("1", itr - 1);
         }
         else if (num.Length % 2 == 0)
         {
             //Console.WriteLine($"Splitting: {num} into  ->  {num.Substring(0, num.Length / 2)}  and  {Regex.Replace(num.Substring(num.Length / 2, num.Length / 2), "^0+(?!$)", "")}");
-            eval(num.Substring(0, num.Length / 2), itr - 1);
-            eval(Regex.Replace(num.Substring(num.Length / 2, num.Length / 2), "^0+(?!$)", ""), itr - 1);
-            count++;  
+            return eval(num.Substring(0, num.Length / 2), itr - 1) +
+                   eval(Regex.Replace(num.Substring(num.Length / 2, num.Length / 2), "^0+(?!$)", ""), itr - 1) +
+                   1;
         }
         else
         {
-            eval((long.Parse(num)*2024).ToString(), itr-1);
+            return eval((long.Parse(num) * 2024).ToString(), itr - 1);
         }
     }
 }
---
+++ AdventOfCode/MyExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        public static TValue TryGetKey<TKey1, TKey2, TValue>(this IDictionary<(TKey1, TKey2), TValue> @this, TKey1 key1, TKey2 key2, Func<TKey1, TKey2, TValue> f)
+            where TKey1 : notnull
+            where TKey2 : notnull
+        {
+            if (@this.ContainsKey((key1, key2)))
+                return @this[(key1, key2)];
+            var newValue = f(key1, key2);
+            @this.Add((key1, key2), newValue);
+            return newValue;
+        }
+    }
+}
